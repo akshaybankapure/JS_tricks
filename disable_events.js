e7c1a5e398ca20359f51e16c1ee5ab117a8b57f6
Akshay Bankapure <hotakshay15@gmail.com>
2021-10-22 14:16:33 +0100

    Update disable_events.js

--- disable_events.js
+++ disable_events.js
@@ -1,7 +1,11 @@
 //This trick is to disable to the alt+F4
-		window.addEventListener('keydown', (e) => {
-    		const { key, altKey } = e;
-    		if (key === 'F4' && altKey) {
-        		e.preventDefault();   
-    		}
-		});
+window.addEventListener('keydown', (e) => {
+   const { key, altKey } = e;
+	if (key === 'F4' && altKey) {
+        e.preventDefault();   
+    	}
+});
+// if the windows has been resized call 'windowResized' to reload the page
+//	window.addEventListener('resize', windowResized);	
+// disable right click context menu
+document.addEventListener('contextmenu', event => event.preventDefault());
